d29b0292521eff60c9a71b84ddaa3a3b60f971f9
rick <philipesferreiraa@gmail.com>
2023-10-18 16:22:10 -0300

    Page pedidos ok e links para pedidos ok

---
+++ pedidos.js
@@ -0,0 +1,36 @@
+import { lerLocalStorage, desenharProdutoCarrinhoSimples } from './src/utilidades';
+
+function criarPedidoHistorico(pedidoComData) {
+  const elementoPedido = ` 
+  <p class="text-sm text-center text-bold mb-4">
+  ${new Date(pedidoComData.dataPedido).toLocaleDateString('pt-BR', {
+    hour: '2-digit',
+    minute: "2-digit"
+  })}
+  </p>
+    <section 
+    id='container-pedidos-${pedidoComData.dataPedido}'>    
+    </section>  
+  `;
+
+  const main = document.getElementsByTagName("main")[0];
+  main.innerHTML += elementoPedido;
+
+  for (const idProduto in pedidoComData.pedido) {
+    desenharProdutoCarrinhoSimples(
+      idProduto,
+      `container-pedidos-${pedidoComData.dataPedido}`,
+      pedidoComData.pedido[idProduto]
+    );
+  }
+}
+
+function renderizarHistoricoPedidos() {
+  const historico = lerLocalStorage('historico')
+  for (const pedidoComData of historico) {
+    criarPedidoHistorico(pedidoComData)
+  }
+}
+
+renderizarHistoricoPedidos()
+
